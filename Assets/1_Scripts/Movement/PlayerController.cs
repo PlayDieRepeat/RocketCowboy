074f8c6a45f8f9cd21c2b6a881919aae2adfc9c6
Jake Gilla <JakeGilla@Tuta.io>
2020-09-21 21:29:04 -0700

    Made the rectangle move using CharacterController

---
+++ Assets/1_Scripts/Movement/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Input;
+
+namespace Rocket.Movement
+{
+    public class PlayerController : MonoBehaviour
+    {
+        CharacterController cmp_controller;
+
+        void Awake()
+        {
+            cmp_controller = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+            
+                Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"),0,0);
+                print(moveVector);
+                cmp_controller.Move(moveVector * Time.deltaTime);
+            
+ 
+        
+        }
+    }
+}
